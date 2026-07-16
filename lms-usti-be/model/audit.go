64106d6c34817caa6dd66e1428bc70f689b35fb1
Muhammad Eagel Triutama <muhammadeagel@gmail.com>
2026-07-16 11:36:47 +0700

    feat: seed script, model text type, gitignore binary

--- lms-usti-be/model/audit.go
+++ lms-usti-be/model/audit.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type AuditLogs struct {
 	gorm.Model
 	Title       string `gorm:"not null"`
-	Description string `gorm:"not null"`
+	Description string `gorm:"type:text;not null"`
 	CreatedBy   string `gorm:"not null"`
 	User        User   `gorm:"foreignKey:CreatedBy;references:ID"`
 }
